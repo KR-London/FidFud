511099c75a3ab462821e20334c0fe0e92afa9d1b
kate <ekaterina.roberts@gmail.com>
2020-05-24 08:41:20 +0100

    initial commit

---
+++ FudFid/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FudFid
+//
+//  Created by Kate Roberts on 24/05/2020.
+//  Copyright © 2020 SaLT for my Squid. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
