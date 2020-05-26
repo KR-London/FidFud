394a29160fb7383aea23aa8790d24d5acf7a7b15
kate <ekaterina.roberts@gmail.com>
2020-05-26 08:29:01 +0100

    including minimal working example and pseudocode for managing firebase storage cache

--- FudFid/Storyboards/testViewController.swift
+++ FudFid/Storyboards/testViewController.swift
@@ -11,108 +11,55 @@ import FirebaseAuth
 import FirebaseStorage
 import ProgressHUD
 import FirebaseUI
+import Foundation
+import FirebaseFirestore
+import Combine
+
+
+enum FirebaseCombineError: Error {
+    case encodeImageFailed
+    case nilResultError
+    case uploadFailed
+}
+
+          let path = String((Bundle.main.path(forResource: "bac2.mov", ofType: nil)?.dropLast(8))!)
+let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first! as NSURL
+var destinationUrl = documentsUrl.appendingPathComponent("bart2.gif")
 
 class testViewController: UIViewController {
 
     @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBAction func ShowmeBart(_ sender: UIButton) {
+        print(destinationUrl)
+         myImageView.image = UIImage.gifImageWithURL(gifUrl: destinationUrl!.absoluteString)
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
+//        myImageView.image = UIImage.gifImageWithName(name: "200.gif")
 //
-//       // myImageView.image = UIImage(contentsOfFile: "https://firebasestorage.googleapis.com/v0/b/fudfid.appspot.com/o/centralGifs%2F200-5.gif?alt=media&token=f590a7c0-995b-4aea-994e-b4f4efa62ea5")
-//
-//        let downloadTask = URL
-//
-//
+//        let storageReference = Storage.storage().reference().child("centralGifs/200w-10.gif")
+//        DispatchQueue.main.async {
+//            // Download to the local filesystem
+//            let downloadTask = storageReference.write(toFile: destinationUrl!) { url, error in
+//                if let error = error {
+//                    // Uh-oh, an error occurred!
+//                    print("Bart has left the building \(String(error.localizedDescription))")
+//                } else {
+//                    print("click to see Bart")
+//                }
+//            }
+//        }
         
-                let storageReference = Storage.storage().reference()
-        
-               // let imageDownloadURLReference = storageReference.child("centralImages/fbtestpic.jpeg")
-        let imageDownloadURLReference = storageReference.child("centralGifs/")
-        var allGifs = [StorageReference]()
-            
-        let fileContents = imageDownloadURLReference.listAll{ (result, error) in
-            if let error = error {
-                // ...
-            }
-            for prefix in result.prefixes {
-                // The prefixes under storageReference.
-                // You may call listAll(completion:) recursively on them.
-            }
-            for item in result.items {
-                allGifs.append(item)
-            }
+        do{
+            try FileManager.default.removeItem(at: destinationUrl!)
         }
-          myImageView.sd_setImage(with: imageDownloadURLReference, placeholderImage: UIImage(named: "peas.jpg"))
-                // Create a reference to the file you want to download
-                //let starsRef = storageRef.child("images/stars.jpg")
-//        for i in 1 ... 15
-//        {
-//                // Fetch the download URL
-//                imageDownloadURLReference.downloadURL { url, error in
-//                    if let error = error {
-//                        print(error.localizedDescription)
-//                    } else {
-//                        print("image url me got is \(String(describing: url!))")
-//                    }
-//                }
-//
-//                // Create local filesystem URL
-//                let path = String((Bundle.main.path(forResource: "bac2.mov", ofType: nil)?.dropLast(8))!)
-//                let localURL = URL(string: "file://" + path + "fbtestpic.jpeg")!
-//
-//                // Download to the local filesystem
-//                let downloadTask = imageDownloadURLReference.write(toFile: localURL) { url, error in
-//                    if let error = error {
-//                        print(error.localizedDescription)
-//                    } else {
-//                        print("Hurrah!")
-//                    }
-//                }
-//
- //       myImageView.sd_setImage(with: imageDownloadURLReference, placeholderImage: UIImage(named: "peas.jpg"))
-       // myImageView.image = UIImage(named: "fbtestpic.jpeg")
-        
-//        let downloadTask = imageDownloadURLReference.write(toFile: localURL) { url, error in
-//                        if let error = error {
-//                            print(error.localizedDescription)
-//                        } else {
-//                            print("Hurrah!")
-//                        }
-//                    }
-//        
-        //.enqueue(<#T##self: StorageDownloadTask##StorageDownloadTask#>)
-//
-//        let storageReference = Storage.storage().reference()
-//
-//        let imageDownloadURLReference = storageReference.child("centralGifs/200-5.gif")
-//
-//        // Create a reference to the file you want to download
-//        //let starsRef = storageRef.child("images/stars.jpg")
-//
-//        // Fetch the download URL
-//        imageDownloadURLReference.downloadURL { url, error in
-//            if let error = error {
-//                print(error.localizedDescription)
-//            } else {
-//                print("image url me got is \(String(describing: url!))")
-//            }
-//        }
-//
-//        // Create local filesystem URL
-//        let path = String((Bundle.main.path(forResource: "bac2.mov", ofType: nil)?.dropLast(8))!)
-//        let localURL = URL(string: "file://" + path + "test.gif")!
-//
-//        // Download to the local filesystem
-//        let downloadTask = imageDownloadURLReference.write(toFile: localURL) { url, error in
-//            if let error = error {
-//                print(error.localizedDescription)
-//            } else {
-//                print("Hurrah!")
-//            }
-//        }
-//
-//        myImageView.image = UIImage.gifImageWithName(name: "test.gif")
-        
+        catch{
+            print(error.localizedDescription)
+        }
     }
 }
+//}
